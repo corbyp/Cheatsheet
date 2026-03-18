0f69a52d8040ca1f9b868673eecf6c37dda95dfb
corby <corbinian.porth@gmx.de>
2026-03-18 03:43:17 +0100

    Removed redundant tests, added test for reverse

--- c/datastructures/dynamic_array/tests/main.c
+++ c/datastructures/dynamic_array/tests/main.c
@@ -12,35 +12,10 @@ void setUp(void) {
 }
 void tearDown(void) { deconstruct(list); }
 
-void test_add_resize(void) {
-  TEST_ASSERT_EQUAL(3, list->add(list, 420));
-
-  TEST_ASSERT_EQUAL(2, list->get(list, 0));
-  TEST_ASSERT_EQUAL(9, list->get(list, 1));
-  TEST_ASSERT_EQUAL(13, list->get(list, 2));
-  TEST_ASSERT_EQUAL(420, list->get(list, 3));
-
-  TEST_ASSERT_EQUAL(4, list->_size);
-  TEST_ASSERT_EQUAL(8, list->_cap);
-}
-
 void test_add(void) {
-  TEST_ASSERT_EQUAL(3, list->add(list, 420));
-  TEST_ASSERT_EQUAL(4, list->add(list, 1337));
-
-  TEST_ASSERT_EQUAL(2, list->get(list, 0));
-  TEST_ASSERT_EQUAL(9, list->get(list, 1));
-  TEST_ASSERT_EQUAL(13, list->get(list, 2));
-  TEST_ASSERT_EQUAL(420, list->get(list, 3));
-  TEST_ASSERT_EQUAL(1337, list->get(list, 4));
-
-  TEST_ASSERT_EQUAL(5, list->_size);
-  TEST_ASSERT_EQUAL(8, list->_cap);
-}
-
-void test_add_many(void) {
   for (int i = 3; i < 32768; ++i) {
     TEST_ASSERT_EQUAL(i, list->add(list, i));
+    TEST_ASSERT_EQUAL(i + 1, list->_size);
   }
 
   for (int i = 3; i < 32768; ++i) {
@@ -54,9 +29,8 @@ void test_add_many(void) {
   TEST_ASSERT_EQUAL(32768 * 2, list->_cap);
 }
 
-void test_insert_resize(void) {
+void test_insert(void) {
   TEST_ASSERT_EQUAL(2, list->insert(list, 420, 2));
-
   TEST_ASSERT_EQUAL(2, list->get(list, 0));
   TEST_ASSERT_EQUAL(9, list->get(list, 1));
   TEST_ASSERT_EQUAL(420, list->get(list, 2));
@@ -64,12 +38,8 @@ void test_insert_resize(void) {
 
   TEST_ASSERT_EQUAL(4, list->_size);
   TEST_ASSERT_EQUAL(8, list->_cap);
-}
-
-void test_insert(void) {
-  TEST_ASSERT_EQUAL(2, list->insert(list, 420, 2));
+  
   TEST_ASSERT_EQUAL(1, list->insert(list, 1337, 1));
-
   TEST_ASSERT_EQUAL(2, list->get(list, 0));
   TEST_ASSERT_EQUAL(1337, list->get(list, 1));
   TEST_ASSERT_EQUAL(9, list->get(list, 2));
@@ -131,17 +101,31 @@ void test_deletei(void) {
   TEST_ASSERT_EQUAL(1, list->_cap);
 }
 
+void test_reverse(void) {
+  TEST_ASSERT_EQUAL(2, list->get(list, 0));
+  TEST_ASSERT_EQUAL(9, list->get(list, 1));
+  TEST_ASSERT_EQUAL(13, list->get(list, 2));
+  TEST_ASSERT_EQUAL(3, list->_size);
+  TEST_ASSERT_EQUAL(4, list->_cap);
+
+  list->reverse(list);
+  
+  TEST_ASSERT_EQUAL(13, list->get(list, 0));
+  TEST_ASSERT_EQUAL(9, list->get(list, 1));
+  TEST_ASSERT_EQUAL(2, list->get(list, 2));
+  TEST_ASSERT_EQUAL(3, list->_size);
+  TEST_ASSERT_EQUAL(4, list->_cap);
+}
+
 int main(void) {
   UNITY_BEGIN();
 
   RUN_TEST(test_add);
-  RUN_TEST(test_add_resize);
-  RUN_TEST(test_add_many);
-  RUN_TEST(test_insert_resize);
   RUN_TEST(test_insert);
   RUN_TEST(test_pop);
   RUN_TEST(test_delete);
   RUN_TEST(test_deletei);
+  RUN_TEST(test_reverse);
 
   return UNITY_END();
 }
