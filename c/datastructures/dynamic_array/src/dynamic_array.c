d9b2eb26a52a5e73488fe10f4ee5036068dc8e47
corby <corbinian.porth@gmx.de>
2026-03-18 03:36:39 +0100

    Added adjust, add, insert, pop, delete, deletei, reverse, get, contains, clear, print, info, construct, deconstruct and relevant tests for add, insert, pop, delete, deletei

--- c/datastructures/dynamic_array/src/dynamic_array.c
+++ c/datastructures/dynamic_array/src/dynamic_array.c
@@ -1,57 +1,183 @@
 #include "dynamic_array.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void add(List *l, int val) {
-  if (l->size + 1 >= l->cap) {
-    int *ptr = reallocarray(l->ptr, l->cap * 2, sizeof(int));
-    if (!ptr) {
-      perror("Could not realloc array!\n");
+void adjust(List *l, const int new_size) {
+  if (new_size == 0) {
+    int *_ptr = reallocarray(l->_ptr, 1, sizeof(int));
+    if (!_ptr) {
+      perror("Could not realloc array with new size 0");
       exit(1);
     }
-    l->ptr = ptr;
-    l->cap *= 2;
+    l->_size = 0;
+    l->_cap = 1;
+  } else if (new_size >= l->_cap) {
+    int *_ptr = reallocarray(l->_ptr, l->_cap * 2, sizeof(int));
+    if (!_ptr) {
+      perror("Could not realloc array on add\n");
+      exit(1);
+    }
+    l->_ptr = _ptr;
+    l->_cap *= 2;
+  } else if (new_size <= l->_cap / 2) {
+    int *_ptr = reallocarray(l->_ptr, l->_cap / 2, sizeof(int));
+    if (!_ptr) {
+      perror("Could not realloc array on delete\n");
+      exit(1);
+    }
+    l->_ptr = _ptr;
+    l->_cap /= 2;
+  }
+}
+
+int add(List *l, const int val) {
+  adjust(l, ++l->_size);
+
+  l->_ptr[l->_size - 1] = val;
+  // Could instead call insert with l->_size as index, but
+  // that would mean one unnecessary if check in while and
+  // thus this implementation is more performant
+
+  return l->_size - 1;
+}
+
+int insert(List *l, const int val, const int idx) {
+  if (idx < 0 || idx > l->_size) {
+    printf("Index %d out of bounds!\n", idx);
+    exit(1);
+  }
+  adjust(l, ++l->_size);
+  int cur = l->_size;
+
+  while (cur > idx) {
+    l->_ptr[cur] = l->_ptr[cur - 1];
+    --cur;
   }
 
-  l->ptr[l->size++] = val;
+  l->_ptr[idx] = val;
+
+  return idx;
 }
 
-void delete(List *l, int val) {
-  int index;
-  for (index = 0; (l->ptr[index] == val || index < l->size); index++) {}
+int pop(List *l) {
+  int val = l->_ptr[--l->_size];
+  adjust(l, l->_size);
 
-  printf("%d\n", index);
+  return val;
 }
 
-void print(List *l) {
+int delete(List *l, const int val) {
+  int idx;
+  for (idx = 0; idx < l->_size && l->_ptr[idx] != val; ++idx)
+    ;
+
+  int temp = idx;
+
+  if (idx == l->_size) {
+    printf("Could not delete element %d, it does not exist!\n", val);
+    exit(1);
+  }
+
+  while (idx < l->_size - 1) {
+    l->_ptr[idx] = l->_ptr[idx + 1];
+    ++idx;
+  }
+
+  adjust(l, --l->_size);
+
+  return temp;
+}
+
+int deletei(List *l, const int idx) {
+  int val = l->get(l, idx);
+
+  int i = idx;
+  while (i < l->_size) {
+    l->_ptr[i] = l->_ptr[i + 1];
+    ++i;
+  }
+
+  adjust(l, --l->_size);
+
+  return val;
+}
+
+void reverse(List *l) {
+  int temp;
+  for (int i = 0; i < l->_size / 2; ++i) {
+    temp = l->_ptr[i];
+    l->_ptr[i] = l->_ptr[l->_size - i - 1];
+    l->_ptr[l->_size - i - 1] = temp;
+  }
+}
+
+int get(const List *l, const int idx) {
+  if (idx < 0 || idx >= l->_size) {
+    printf("Index %d out of bounds!\n", idx);
+    exit(1);
+  }
+  return l->_ptr[idx];
+}
+
+int contains(const List *l, const int val) {
+  for (int i = 0; i < l->_size; ++i) {
+    if (l->_ptr[i] == val)
+      return 1;
+  }
+  return 0;
+}
+
+void clear(List *l) { adjust(l, 0); }
+
+void print(const List *l) {
+  if (l->_size == 0) {
+    printf("[]\n");
+    return;
+  }
   printf("[");
-  for (int i = 0; i < l->size - 1; ++i) {
-    printf("%d, ", l->ptr[i]);
+  for (int i = 0; i < l->_size - 1; ++i) {
+    printf("%d, ", l->_ptr[i]);
   }
-  printf("%d], c: %d, s: %d\n", l->ptr[l->size - 1], l->cap, l->size);
+  printf("%d]\n", l->_ptr[l->_size - 1]);
 }
 
-List *init(void) {
+void info(const List *l) {
+  printf("Capacity: %d, Size: %d\n", l->_cap, l->_size);
+}
+
+void deconstruct(List *l) {
+  free(l->_ptr);
+  // free(l);
+}
+
+List *construct(void) {
   List *l = calloc(1, sizeof(List));
   if (!l) {
-    perror("Could not alloc list!\n");
+    perror("Could not alloc list");
     exit(1);
   }
 
   l->add = add;
+  l->insert = insert;
+  l->pop = pop;
   l->delete = delete;
+  l->deletei = deletei;
+  l->reverse = reverse;
+  l->get = get;
+  l->contains = contains;
   l->print = print;
+  l->info = info;
 
-  int *ptr = calloc(1, sizeof(int));
-  if (!ptr) {
-    perror("Could not malloc array!\n");
+  int *_ptr = calloc(1, sizeof(int));
+  if (!_ptr) {
+    perror("Could not alloc array");
     exit(1);
   }
 
-  l->ptr = ptr;
-  l->cap = 1;
+  l->_ptr = _ptr;
+  l->_cap = 1;
 
   return l;
 }
--- c/datastructures/dynamic_array/tests/main.c
+++ c/datastructures/dynamic_array/tests/main.c
@@ -4,27 +4,144 @@
 
 List *list;
 
-void setUp(void) { list = init(); }
-void tearDown(void) {}
+void setUp(void) {
+  list = construct();
+  list->add(list, 2);
+  list->add(list, 9);
+  list->add(list, 13);
+}
+void tearDown(void) { deconstruct(list); }
 
-void test_add_one(void) {
+void test_add_resize(void) {
+  TEST_ASSERT_EQUAL(3, list->add(list, 420));
+
+  TEST_ASSERT_EQUAL(2, list->get(list, 0));
+  TEST_ASSERT_EQUAL(9, list->get(list, 1));
+  TEST_ASSERT_EQUAL(13, list->get(list, 2));
+  TEST_ASSERT_EQUAL(420, list->get(list, 3));
+
+  TEST_ASSERT_EQUAL(4, list->_size);
+  TEST_ASSERT_EQUAL(8, list->_cap);
+}
+
+void test_add(void) {
+  TEST_ASSERT_EQUAL(3, list->add(list, 420));
+  TEST_ASSERT_EQUAL(4, list->add(list, 1337));
+
+  TEST_ASSERT_EQUAL(2, list->get(list, 0));
+  TEST_ASSERT_EQUAL(9, list->get(list, 1));
+  TEST_ASSERT_EQUAL(13, list->get(list, 2));
+  TEST_ASSERT_EQUAL(420, list->get(list, 3));
+  TEST_ASSERT_EQUAL(1337, list->get(list, 4));
+
+  TEST_ASSERT_EQUAL(5, list->_size);
+  TEST_ASSERT_EQUAL(8, list->_cap);
 }
 
 void test_add_many(void) {
+  for (int i = 3; i < 32768; ++i) {
+    TEST_ASSERT_EQUAL(i, list->add(list, i));
+  }
+
+  for (int i = 3; i < 32768; ++i) {
+    TEST_ASSERT_EQUAL(i, list->get(list, i));
+  }
+
+  TEST_ASSERT_EQUAL(2, list->get(list, 0));
+  TEST_ASSERT_EQUAL(9, list->get(list, 1));
+  TEST_ASSERT_EQUAL(13, list->get(list, 2));
+  TEST_ASSERT_EQUAL(32768, list->_size);
+  TEST_ASSERT_EQUAL(32768 * 2, list->_cap);
 }
 
-void other(void) {
-  list = init();
-  printf("Here\n");
+void test_insert_resize(void) {
+  TEST_ASSERT_EQUAL(2, list->insert(list, 420, 2));
+
+  TEST_ASSERT_EQUAL(2, list->get(list, 0));
+  TEST_ASSERT_EQUAL(9, list->get(list, 1));
+  TEST_ASSERT_EQUAL(420, list->get(list, 2));
+  TEST_ASSERT_EQUAL(13, list->get(list, 3));
+
+  TEST_ASSERT_EQUAL(4, list->_size);
+  TEST_ASSERT_EQUAL(8, list->_cap);
+}
+
+void test_insert(void) {
+  TEST_ASSERT_EQUAL(2, list->insert(list, 420, 2));
+  TEST_ASSERT_EQUAL(1, list->insert(list, 1337, 1));
+
+  TEST_ASSERT_EQUAL(2, list->get(list, 0));
+  TEST_ASSERT_EQUAL(1337, list->get(list, 1));
+  TEST_ASSERT_EQUAL(9, list->get(list, 2));
+  TEST_ASSERT_EQUAL(420, list->get(list, 3));
+  TEST_ASSERT_EQUAL(13, list->get(list, 4));
+
+  TEST_ASSERT_EQUAL(5, list->_size);
+  TEST_ASSERT_EQUAL(8, list->_cap);
+}
+
+void test_pop(void) {
+  TEST_ASSERT_EQUAL(13, list->pop(list));
+  TEST_ASSERT_EQUAL(2, list->get(list, 0));
+  TEST_ASSERT_EQUAL(9, list->get(list, 1));
+  TEST_ASSERT_EQUAL(2, list->_size);
+  TEST_ASSERT_EQUAL(2, list->_cap);
+
+  TEST_ASSERT_EQUAL(9, list->pop(list));
+  TEST_ASSERT_EQUAL(2, list->get(list, 0));
+  TEST_ASSERT_EQUAL(1, list->_size);
+  TEST_ASSERT_EQUAL(1, list->_cap);
+
+  TEST_ASSERT_EQUAL(2, list->pop(list));
+  TEST_ASSERT_EQUAL(0, list->_size);
+  TEST_ASSERT_EQUAL(1, list->_cap);
+}
+
+void test_delete(void) {
+  TEST_ASSERT_EQUAL(1, list->delete(list, 9));
+  TEST_ASSERT_EQUAL(2, list->get(list, 0));
+  TEST_ASSERT_EQUAL(13, list->get(list, 1));
+  TEST_ASSERT_EQUAL(2, list->_size);
+  TEST_ASSERT_EQUAL(2, list->_cap);
+
+  TEST_ASSERT_EQUAL(0, list->delete(list, 2));
+  TEST_ASSERT_EQUAL(13, list->get(list, 0));
+  TEST_ASSERT_EQUAL(1, list->_size);
+  TEST_ASSERT_EQUAL(1, list->_cap);
+
+  TEST_ASSERT_EQUAL(0, list->delete(list, 13));
+  TEST_ASSERT_EQUAL(0, list->_size);
+  TEST_ASSERT_EQUAL(1, list->_cap);
+}
+
+void test_deletei(void) {
+  TEST_ASSERT_EQUAL(9, list->deletei(list, 1));
+  TEST_ASSERT_EQUAL(2, list->get(list, 0));
+  TEST_ASSERT_EQUAL(13, list->get(list, 1));
+  TEST_ASSERT_EQUAL(2, list->_size);
+  TEST_ASSERT_EQUAL(2, list->_cap);
+
+  TEST_ASSERT_EQUAL(2, list->deletei(list, 0));
+  TEST_ASSERT_EQUAL(13, list->get(list, 0));
+  TEST_ASSERT_EQUAL(1, list->_size);
+  TEST_ASSERT_EQUAL(1, list->_cap);
+
+  TEST_ASSERT_EQUAL(13, list->deletei(list, 0));
+  TEST_ASSERT_EQUAL(0, list->_size);
+  TEST_ASSERT_EQUAL(1, list->_cap);
 }
 
 int main(void) {
   UNITY_BEGIN();
 
-  RUN_TEST(test_add_one);
+  RUN_TEST(test_add);
+  RUN_TEST(test_add_resize);
   RUN_TEST(test_add_many);
-
-  other();
+  RUN_TEST(test_insert_resize);
+  RUN_TEST(test_insert);
+  RUN_TEST(test_pop);
+  RUN_TEST(test_delete);
+  RUN_TEST(test_deletei);
 
   return UNITY_END();
 }
